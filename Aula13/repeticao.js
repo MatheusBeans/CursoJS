07ce51e48b31459063d20be626666f0c72cf98e0
Matheus {Fejao} <matheus.souza.fjo@gmail.com>
2022-03-24 11:02:57 -0300

    inicio-repeticoes

---
+++ Aula13/repeticao.js
@@ -0,0 +1,18 @@
+var c = 0
+
+//teste no inicio
+/*while(c<6){
+    console.log('tudo bem?')
+    c++
+}*/
+
+//teste no final
+/*do{
+    console.log('tudo bem?')
+    c++
+} while (c<6)*/
+
+//teste enquanto "for"
+for(var c=1; c<=10; c++){
+    console.log(`Test ${c}`)
+}
